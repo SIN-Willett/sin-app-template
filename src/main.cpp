73bf03694aefd54c53cf198682f1881f05327f83
Sioryn Indianna Nereyeus  Willett <s4392620@student.uq.edu.au>
2022-01-10 15:04:58 +1000

    now finds and includes the library template

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+/**
+ ***************************************************************
+ * @file main.cpp 
+ * @author Sioryn Willett
+ * @date 2022-01-10
+ * @brief //TODO
+ ***************************************************************
+ */
+extern "C" {
+#include <lib.h>
+}
+
+int main(int argc, char** argv){
+    return lib(3) != 6;
+}
